d27fe649e3bae33fc235b692d06949b5426f8a4f
michi <chischte@gmx.ch>
2020-12-21 22:09:30 +0100

    Complete SudokuManager tests

--- SudokuApp.Test/SudokuManagerTests.cs
+++ SudokuApp.Test/SudokuManagerTests.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SudokuApp.Model;
 
@@ -143,7 +144,7 @@ namespace SudokuApp
 
             inputStringArray[60] = "7"; // Sector 9 / Row 7 / Column 7
             inputStringArray[70] = "7"; // Sector 9 / Row 8 / Column 8
-            inputStringArray[50] = "7"; // Sector 5 -> not a duplicate
+            inputStringArray[50] = "7"; // Sector 5 / Row 6 / Column 6 -> not a duplicate
 
 
             FieldColor expectedResultDuplicate = FieldColor.Red; // Duplicates are marked red
@@ -160,8 +161,30 @@ namespace SudokuApp
             Assert.AreEqual(expectedResultNoDuplicate, result[50]);
         }
 
+        [TestMethod]
+        public void TestCompleteSudokuReturnsOnlyGreenColor()
+        {
+            // Arrange
+            string[] inputStringArray = new string[]
+            {
+                "5","3","4","6","7","8","9","1","2",
+                "6","7","2","1","9","5","3","4","8",
+                "1","9","8","3","4","2","5","6","7",
+                "8","5","9","7","6","1","4","2","3",
+                "4","2","6","8","5","3","7","9","1",
+                "7","1","3","9","2","4","8","5","6",
+                "9","6","1","5","3","7","2","8","4",
+                "2","8","7","4","1","9","6","3","5",
+                "3","4","5","2","8","6","1","7","9"
+            };
 
+            // Act
+            var result = this.testee.GetFieldColorArray(inputStringArray);
 
-
+            // Assert
+            Assert.AreEqual(false, result.Contains(FieldColor.Black));
+            Assert.AreEqual(false, result.Contains(FieldColor.Red));
+            Assert.AreEqual(true, result.Contains(FieldColor.Green));
+        }
     }
 }
