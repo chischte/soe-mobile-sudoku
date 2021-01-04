ccc3c3e14b491c8c988d55982cbb9d8093471a0d
michi <chischte@gmx.ch>
2021-01-04 13:42:49 +0100

    Morphing one- and two-dimensional arrays work

--- SudokuApp/SudokuApp/Solver/SudokuSolver.cs
+++ SudokuApp/SudokuApp/Solver/SudokuSolver.cs
@@ -11,14 +11,46 @@ namespace SudokuApp.Solver
 
         public int[] GetSolvedSudoku(int[] sudokuToSolve)
         {
-            int[] solvedSudoku = new int[NumberOfSudokuFields];
+            int[,] twoDimensionalSudoku = GenerateTwoDimensionalArray(sudokuToSolve);
+            int[] solvedSudoku = GenerateOneDimensionalArray(twoDimensionalSudoku);
             return solvedSudoku;
         }
 
-        private int[,] GenerateTwoDimensionalArray(int[] sudokuToSolve)
+        private int[,] GenerateTwoDimensionalArray(int[] oneDimensionalArray)
         {
-            int[,] twoDimensionalSudoku = new int[SudokuSquareLength,SudokuSquareLength];
-            return twoDimensionalSudoku;
+            int[,] twoDimensionalArray = new int[SudokuSquareLength, SudokuSquareLength];
+            int row = 0;
+            int column = 0;
+            for (int i = 0; i < NumberOfSudokuFields; i++)
+            {
+                twoDimensionalArray[row, column] = oneDimensionalArray[i];
+                column++;
+                if (column == SudokuSquareLength)
+                {
+                    row++;
+                    column = 0;
+                }
+            }
+            return twoDimensionalArray;
+        }
+
+        private int[] GenerateOneDimensionalArray(int[,] twoDimensionalArray)
+        {
+            int[] oneDimensionalArray = new int[NumberOfSudokuFields];
+            int row = 0;
+            int column = 0;
+            for (int i = 0; i < NumberOfSudokuFields; i++)
+            {
+                oneDimensionalArray[i] = twoDimensionalArray[row, column];
+                column++;
+                if (column == SudokuSquareLength)
+                {
+                    row++;
+                    column = 0;
+                }
+
+            }
+            return oneDimensionalArray;
         }
     }
 }
