7786fe90bf6f49b4cabd39766afae6f2c94a2c8f
michi <chischte@gmx.ch>
2021-01-04 13:01:52 +0100

    Start implementation of solver

---
+++ SudokuApp/SudokuApp/Solver/SudokuSolver.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SudokuApp.Solver
+{
+    class SudokuSolver
+    {
+        const int NumberOfSudokuFields = 81;
+        const int SudokuSquareLength = 9;
+
+        public int[] GetSolvedSudoku(int[] sudokuToSolve)
+        {
+
+
+            int[] solvedSudoku = new int[NumberOfSudokuFields];
+            return solvedSudoku;
+        }
+
+        private int[][] GenerateTwoDimensionalArray(int[] sudokuToSolve)
+        {
+            int[][] TwoDimensionalSudoku = new int[SudokuSquareLength][SudokuSquareLength];
+            return TwoDimensionalSudoku;
+        }
+    }
+}
